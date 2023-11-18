4590767ab596eb3f835cdfef417bc5cac0981e40
alext2052 <alextong2004@gmail.com>
2023-11-18 01:19:31 -0600

    Move to 480 adc cycles

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -266,7 +266,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_7;
   sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_56CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
