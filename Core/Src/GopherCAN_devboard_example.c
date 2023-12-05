e3396d32de3cc5f7e1d758ef814f03e5398505ea
alext2052 <alextong2004@gmail.com>
2023-12-04 22:21:20 -0600

    Fix conversion ratio

--- Core/Src/GopherCAN_devboard_example.c
+++ Core/Src/GopherCAN_devboard_example.c
@@ -7,9 +7,8 @@
 #include "pulse_sensor.h"
 #include "gopher_sense.h"
 
-#define CONVERSION_RATIO (float)CALCULATE_MPH_CONVERSION_RATIO(30.0f, 7.8f) // Conversion ration from frequency of pulses to mph
+#define CONVERSION_RATIO 4.975 // Conversion ration from frequency of pulses to GPerSec
 #define HDMA_CHANNEL_4 2 // hdma value dma is going to use
-#define HDMA_CHANNEL_3 3 // TODO: This is prob wrong, verify
 #define DMA_STOPPED_TIMEOUT_MS 1000
 #define LOW_PULSES_PER_SECOND 1 // 15 mph, when we only take 5 samples per dma check
 #define HIGH_PULSES_PER_SECOND 300 // Don't expect to reach this but we don't want to take that many samples
